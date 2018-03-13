a3372c9d393ec991059b1f092647e27e74110b69
chiavy <chiavy@163.com>
2018-03-13 15:57:59 +0800

    Initial commit

---
+++ app/src/main/java/com/myzhang/ZhangMainActivity.java
@@ -0,0 +1,19 @@
+package com.myzhang;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ZhangMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void haha(View view){
+//        Intent intent = new Intent(this, ZMainActivity.class);
+//        startActivity(intent);
+    }
+}
